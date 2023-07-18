832158ac72d01654e4d3a6e938b93b5067827b5e
BILLMURRAYY <nitipattime@gmail.com>
2023-07-18 19:42:47 +0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
 import Album from './components/Album'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <>
       <Header />
+      <Nav />
       <Album />
     </>
   )
